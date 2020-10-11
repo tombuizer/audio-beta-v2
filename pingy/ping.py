3a3b460c02beb530491a83ef4f29352408506856
tom <contact@tombuizer.nl>
2020-10-11 14:27:43 +0200

    Update ping.py

--- pingy/ping.py
+++ pingy/ping.py
@@ -8,6 +8,23 @@ from redbot.core.bot import Red
 from redbot.core.commands import Cog
 from redbot.core.utils.chat_formatting import pagify
 
+
+        
+        class Pingy(Cog):
+    """
+    V3 Cog Template
+    """
+
+    def __init__(self, bot: Red):
+        super().__init__()
+        self.bot = bot
+        self.config = Config.get_conf(self, identifier=9811198108111121, force_registration=True)
+        default_global = {}
+        default_guild = {}
+
+        self.config.register_global(**default_global)
+        self.config.register_guild(**default_guild)
+
 class PingRepeat(menus.Menu):
     
     def __init__(self,bot:commands.Bot, p: int, original_ctx: commands.Context):
