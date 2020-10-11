89e7349bb657973e367602b04fde128854636cdc
tom <contact@tombuizer.nl>
2020-10-11 14:39:10 +0200

    added 7x

--- latency/mycog.py
+++ latency/mycog.py
@@ -4,7 +4,7 @@ class Mycog(commands.Cog):
     """My custom cog"""
 
     @commands.command()
-    async def mycom(self, ctx):
+    async def 7x(self, ctx):
         """This does stuff!"""
         # Your code will go here
-        await ctx.send("I can do stuff!")
+        await ctx.send("<@738981683516145785>")
